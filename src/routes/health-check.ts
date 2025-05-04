b281bae1631d9f685fb8646f3bee1e7adf5190fa
Rodrigo Marques Hitss <marques.coti@gmail.com>
2025-05-03 22:29:30 -0300

    ECS por github actions

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -3,6 +3,6 @@ import type { FastifyInstance } from "fastify";
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
 
-    await reply.status(200).send({ message: 'ok' })
+    await reply.status(200).send({ message: 'Tudo ok no ECS' })
   })
 }
